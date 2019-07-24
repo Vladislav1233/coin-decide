e86d9804d9dc9cb09f509ea88e3f06335e1e93a4
vladka.dovzh@gmail.com <vladka.dovzh@gmail.com>
2019-07-25 01:07:36 +0400

    add shaking

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
-import Sprite from 'components/Coin/';
 import Shake from 'shake2.js';
+import launchFlipCoin from 'helpers/launchFlipCoin';
 
 class App extends Component {
 
   componentDidMount() {
+
     // Create an instance
     const myShakeEvent = new Shake({
       threshold: 15, // optional shake strength threshold
@@ -20,40 +21,13 @@ class App extends Component {
 
     // Dunction to call when shake event occurs
     function shakeEventDidOccur () {
-      let canvas = document.getElementById('canvas');
-      canvas.width = 100;
-      canvas.height = 100;
-      let coinImage = new Image();
-      coinImage.src = 'http://www.williammalone.com/articles/create-html5-canvas-javascript-sprite-animation/downloads/sprite-animation-demo/images/coin-sprite-animation.png';
-      // eslint-disable-next-line
-      let sprite = new Sprite({
-          ctx: canvas.getContext('2d'),
-          image: coinImage,
-          width: 1000,
-          height: 100,
-          numberOfFrames: 10,
-          ticksPerFrame: -15,
-      });
+      launchFlipCoin();
     }
   }
 
   render() {
 
-    let canvas = document.getElementById('canvas');
-      canvas.width = 100;
-      canvas.height = 100;
-      let coinImage = new Image();
-      coinImage.src = 'http://www.williammalone.com/articles/create-html5-canvas-javascript-sprite-animation/downloads/sprite-animation-demo/images/coin-sprite-animation.png';
-
-    // eslint-disable-next-line
-    let sprite = new Sprite({
-      ctx: canvas.getContext('2d'),
-      image: coinImage,
-      width: 1000,
-      height: 100,
-      numberOfFrames: 0,
-      ticksPerFrame: 0,
-    });
+    launchFlipCoin();
 
     return (
       <div className="App">
