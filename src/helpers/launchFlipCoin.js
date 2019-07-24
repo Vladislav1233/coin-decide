4dbdfc47add4a9bcf8c2a8c86e4d9b3ee9725a02
vladka.dovzh@gmail.com <vladka.dovzh@gmail.com>
2019-07-25 01:01:07 +0400

    shake oin

---
+++ src/helpers/launchFlipCoin.js
@@ -0,0 +1,36 @@
+import Shake from 'shake2.js';
+import Sprite from 'components/Coin/';
+
+const launchFlipCoin = (numFrame, ticksFrame) => {
+  // Create an instance
+  const myShakeEvent = new Shake({
+    threshold: 15, // optional shake strength threshold
+    timeout: 1000 // optional, determines the frequency of event generation
+  });
+
+  // Start listening to device motion:
+  myShakeEvent.start();
+
+  // Register a `shake` event listener on `window` with your callback:
+  window.addEventListener('shake', shakeEventDidOccur, false);
+
+  // Dunction to call when shake event occurs
+  function shakeEventDidOccur () {
+    let canvas = document.getElementById('canvas');
+    canvas.width = 100;
+    canvas.height = 100;
+    let coinImage = new Image();
+    coinImage.src = 'http://www.williammalone.com/articles/create-html5-canvas-javascript-sprite-animation/downloads/sprite-animation-demo/images/coin-sprite-animation.png';
+    // eslint-disable-next-line
+    let sprite = new Sprite({
+        ctx: canvas.getContext('2d'),
+        image: coinImage,
+        width: 1000,
+        height: 100,
+        numberOfFrames: numFrame || 1,
+        ticksPerFrame: ticksFrame || 0,
+    });
+  }
+};
+
+export default launchFlipCoin;
