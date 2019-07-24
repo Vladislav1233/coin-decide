f8cf7030affdffcabc6892b4cc899371afd37696
vladka.dovzh@gmail.com <vladka.dovzh@gmail.com>
2019-07-25 01:15:19 +0400

    Updates

---
+++ precache-manifest.87fdf44cc88d64994f8c80ad4e89345f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2b99adc80d8526d2e537d0783b4e64",
+    "url": "/coin-decide/index.html"
+  },
+  {
+    "revision": "895bdabea306e780e03f",
+    "url": "/coin-decide/static/css/main.ddf7169d.chunk.css"
+  },
+  {
+    "revision": "8fdbf8cf4c32994f1ca9",
+    "url": "/coin-decide/static/js/2.16610868.chunk.js"
+  },
+  {
+    "revision": "895bdabea306e780e03f",
+    "url": "/coin-decide/static/js/main.63d9d378.chunk.js"
+  },
+  {
+    "revision": "b78251d90acc420129f1",
+    "url": "/coin-decide/static/js/runtime~main.cf3ffb2c.js"
+  }
+]);
