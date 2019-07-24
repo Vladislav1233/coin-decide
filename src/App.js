ae82d7d46a2f158d8356578d44f469494921a730
vladka.dovzh@gmail.com <vladka.dovzh@gmail.com>
2019-07-24 16:28:51 +0400

    coin in component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import './App.scss';
-// import Sprite from 'components/Coin/';
+import Sprite from 'components/Coin/';
+
 class App extends Component {
 
   render() {
+    let canvas = document.getElementById('canvas');
+    canvas.width = 100;
+    canvas.height = 100;
+    let coinImage = new Image();
+    coinImage.src = 'http://www.williammalone.com/articles/create-html5-canvas-javascript-sprite-animation/downloads/sprite-animation-demo/images/coin-sprite-animation.png';
+    let sprite = new Sprite({
+        ctx: canvas.getContext('2d'),
+        image: coinImage,
+        width: 1000,
+        height: 100,
+        numberOfFrames: 10,
+        ticksPerFrame: -10,
+    })
 
     return (
       <div className="App">
---
+++ src/components/Coin/index.js
@@ -0,0 +1,63 @@
+class Sprite {
+  constructor(options) {
+    this.ctx = options.ctx;
+
+    this.image = options.image;
+    this.countLoop = 0;
+
+    this.frameIndex = 0;
+    this.tickCount = 0;
+    this.ticksPerFrame = options.ticksPerFrame || 0;
+    this.numberOfFrames = options.numberOfFrames || 1;
+
+    this.width = options.width;
+    this.height = options.height;
+
+    this.start();
+  }
+
+  update() {
+    this.tickCount++;
+
+    if (this.tickCount > this.ticksPerFrame) {
+      this.tickCount = 0;
+      if (this.frameIndex < this.numberOfFrames - 1) {
+        this.frameIndex++;
+      } else {
+        this.frameIndex = 0;
+        this.countLoop++;
+        this.ticksPerFrame++;
+      }
+    }
+  }
+
+  render() {
+    this.ctx.clearRect(0, 0, this.width / this.numberOfFrames, this.height);
+    this.ctx.drawImage(
+      this.image,
+      this.frameIndex * this.width / this.numberOfFrames,
+      0,
+      this.width / this.numberOfFrames,
+      this.height,
+      0,
+      0,
+      this.width / this.numberOfFrames,
+      this.height
+    )
+  }
+
+  start() {
+    let loop = () => {
+      this.update();
+      this.render();
+
+      if (this.countLoop <= 15) {
+        window.requestAnimationFrame(loop);
+      }
+    }
+
+    window.requestAnimationFrame(loop);
+  }
+}
+
+export default Sprite;
