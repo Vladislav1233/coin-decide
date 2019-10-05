059abde8bb2421daed0d7d0954012ebe62e1b0ae
vladka.dovzh@gmail.com <vladka.dovzh@gmail.com>
2019-10-05 21:24:24 +0400

    version_2: promocode

--- src/components/PromocodeList/index.js
+++ src/components/PromocodeList/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
+import { Link } from 'react-router-dom';
 
 // Note: Components
 import PromocodeItem from './PromocodeItem';
@@ -35,23 +36,26 @@ const data = [{
 class PromocodeList extends Component {
   render() {
     console.log(this.props)
+    const { auth } = this.props;
     const promocodes = this.props.promocodes ? this.props.promocodes.content : [];
     return(
       <ul className="b-promocode-list">
-        {promocodes.map((promocode, index) => (
+        {!!auth.uid ? promocodes.map((promocode, index) => (
           <li key={index} className="b-promocode-list__item">
             <PromocodeItem
               {...promocode}
             />
           </li>
-        ))}
+        )) : <li className="b-promocode-list__registration">
+          <Link to='/signin'>Войдите</Link>
+          <div className="b-promocode-list__help">чтобы видеть все свои промокоды</div>
+        </li>}
       </ul>
     )
   }
 }
 
 const mapStateToProps = ({ firestore, firebase }) => {
-  console.log(firestore)
   return {
     promocodes: firestore.ordered.promocodes ? firestore.ordered.promocodes[0] : firestore.ordered.promocodes,
     auth: firebase.auth
@@ -62,7 +66,7 @@ export default compose(
   connect(mapStateToProps),
   // ref doc: https://github.com/prescottprue/redux-firestore#query-options%23query-options
   firestoreConnect(props => {
-    const dataQuery = [];
+    let dataQuery = [];
     if (props.auth.uid) dataQuery.push({
       collection: 'promocodes',
       doc: props.auth.uid,
