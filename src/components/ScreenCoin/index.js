0c34013b8039b6cc2ea7c889bb79c6014fe4f07e
Amed <amedomaryyo93@mail.ru>
2019-10-06 21:51:29 +0400

    some fix slider

--- src/components/ScreenCoin/index.js
+++ src/components/ScreenCoin/index.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import { isMobileOnly } from 'react-device-detect';
-import Slider from 'react-slick';
-import { Player, ControlBar } from 'video-react';
-import 'video-react/dist/video-react.css';
-import { Swipeable } from 'react-swipeable';
+import Slider from "react-slick";
+import { Player, ControlBar } from "video-react";
+import "video-react/dist/video-react.css";
+import { Swipeable } from "react-swipeable";
 // import launchFlipCoin from 'helpers/launchFlipCoin';
 
 // Note: media
-import v from 'images/coin-v.mp4';
-import settingIcon from 'images/setting.svg';
-import list from 'images/list.svg';
-import swipeImg from 'images/swipe.svg';
-import arrowSideIcon from 'images/arrow-side.svg';
+import v from "images/coin-v.mp4";
+import settingIcon from "images/setting.svg";
+import list from "images/list.svg";
+import swipeImg from "images/swipe.svg";
+import arrowSideIcon from "images/arrow-side.svg";
 
 // Note: components
-import PromocodeList from 'components/PromocodeList';
-import SettingList from 'components/SettingList';
+import PromocodeList from "components/PromocodeList";
+import SettingList from "components/SettingList";
 // import Button from 'components/Button';
 
-import './style.scss';
+import "./style.scss";
 
-navigator.geolocation.getCurrentPosition((pos) => {
-  console.log(pos)
-})
+navigator.geolocation.getCurrentPosition(pos => {
+  console.log(pos);
+});
 
 class ScreenCoin extends Component {
-
   state = {
     flipEnded: false,
     isCoinToss: false
@@ -34,13 +33,13 @@ class ScreenCoin extends Component {
   componentDidMount() {
     // Note: subscribe state change on player
     this.player.subscribeToStateChange(this.handleStateChange.bind(this));
-  };
+  }
 
   handleStateChange(state, prevState) {
     if (state.ended) {
       this.props.stopFlipping();
     }
-  };
+  }
 
   // TODO: shake событие
   // onClickFlip = () => {
@@ -71,20 +70,23 @@ class ScreenCoin extends Component {
       adaptiveHeight: true,
       initialSlide: 1,
       arrows: false,
-      fade: true,
+      fade: false,
       slidesToShow: 1,
       slidesToScroll: 1
     };
 
-    return(
-      <div className={`b-screen-coin ${isCoinToss ? 'is-coin-toss' : ''}`}>
-        <Slider ref={c => (this.slider = c)} { ...sliderSetting }>
+    return (
+      <div className={`b-screen-coin ${isCoinToss ? "is-coin-toss" : ""}`}>
+        <Slider ref={c => (this.slider = c)} {...sliderSetting}>
           <div className="b-screen-coin__slide">
             <h2 className="b-screen-coin__title">Настройки</h2>
 
             <div className="b-coin-head">
-              <div className="b-coin-head__item b-coin-head__item--right">
-                <div className="b-coin-head__icon b-coin-head__icon--right" onClick={this.nextSlide}>
+              <div
+                className="b-coin-head__item b-coin-head__item--right"
+                onClick={this.nextSlide}
+              >
+                <div className="b-coin-head__icon b-coin-head__icon--right">
                   <img src={arrowSideIcon} alt="" />
                 </div>
               </div>
@@ -94,33 +96,51 @@ class ScreenCoin extends Component {
           </div>
 
           <div className="b-screen-coin__slide">
-            <div className={`b-coin-head ${isCoinToss ? 'is-hide' : ''}`}>
-              <button className="b-coin-head__item" type="button">
-                <div className="b-coin-head__icon" onClick={this.previousSlide}>
+            <div className={`b-coin-head ${isCoinToss ? "is-hide" : ""}`}>
+              <button
+                className="b-coin-head__item"
+                type="button"
+                onClick={this.previousSlide}
+              >
+                <div className="b-coin-head__icon">
                   <img src={settingIcon} alt="" />
                 </div>
               </button>
 
-              <button className="b-coin-head__item" type="button">
-                <div className="b-coin-head__icon" onClick={this.nextSlide}>
+              <button
+                className="b-coin-head__item"
+                type="button"
+                onClick={this.nextSlide}
+              >
+                <div className="b-coin-head__icon">
                   <img src={list} alt="" />
                 </div>
               </button>
             </div>
 
-            <Swipeable className="b-screen-coin__swipeable" onSwipedUp={this.onSwipedUp} preventDefaultTouchmoveEvent={true}>
-              <div className={`b-screen-coin__control-image ${isCoinToss ? 'is-hide' : ''}`}>
+            <Swipeable
+              className="b-screen-coin__swipeable"
+              onSwipedUp={this.onSwipedUp}
+              preventDefaultTouchmoveEvent={true}
+            >
+              <div
+                className={`b-screen-coin__control-image ${
+                  isCoinToss ? "is-hide" : ""
+                }`}
+              >
                 <img src={swipeImg} alt="" />
                 <div></div>
               </div>
 
               <Player
-                ref={(player) => { this.player = player }}
+                ref={player => {
+                  this.player = player;
+                }}
                 fluid={false}
                 width="100%"
                 height="100%"
               >
-                <source src={v}/>
+                <source src={v} />
                 <ControlBar disableCompletely={true} />
               </Player>
             </Swipeable>
@@ -130,8 +150,8 @@ class ScreenCoin extends Component {
             <h2 className="b-screen-coin__title">Мои промокоды</h2>
 
             <div className="b-coin-head">
-              <div className="b-coin-head__item">
-                <div className="b-coin-head__icon" onClick={this.previousSlide}>
+              <div className="b-coin-head__item" onClick={this.previousSlide}>
+                <div className="b-coin-head__icon">
                   <img src={arrowSideIcon} alt="" />
                 </div>
               </div>
@@ -141,7 +161,7 @@ class ScreenCoin extends Component {
           </div>
         </Slider>
       </div>
-    )
+    );
   }
 }
 
