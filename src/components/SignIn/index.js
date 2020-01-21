9637f394b4c3e30581a525e29f5c128032ce1517
Max Grishin <amed@ss.com>
2020-01-21 10:58:45 +0400

    add bem class

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { signIn } from "store/auth";
+import bem from "config/bem";
 
 // Note: styles
 import "./style.scss";
+
+const bemClass = bem('sign')
 
 class SignIn extends Component {
   state = {
@@ -25,21 +28,21 @@ class SignIn extends Component {
   render() {
     console.log(this.props.authError);
     return (
-      <main>
+      <main className={bemClass()}>
         <svg
-          className="sign__bg sign__bg--top"
+          className={bemClass('bg', {top: true})}
           width="353"
           height="202"
           viewBox="0 0 353 202"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <g clip-path="url(#clip0)">
+          <g clipPath="url(#clip0)">
             <path
               d="M5.58143 186.402C-16.7847 207.411 -42.623 207.145 -52.7464 204.386L-155.265 132.124L8.44741 -126.602L371.796 -86.1431C389.379 -90.0134 418.431 -86.5454 393.968 -41.7113C363.389 14.3314 212.039 12.1655 119.967 39.6799C27.8952 67.1944 33.5391 160.141 5.58143 186.402Z"
               fill="url(#paint0_linear)"
               stroke="#21372A"
-              stroke-width="10"
+              strokeWidth="10"
             />
           </g>
           <defs>
@@ -51,8 +54,8 @@ class SignIn extends Component {
               y2="209.205"
               gradientUnits="userSpaceOnUse"
             >
-              <stop stop-color="#09C883" />
-              <stop offset="1" stop-color="#086A29" />
+              <stop stopColor="#09C883" />
+              <stop offset="1" stopColor="#086A29" />
             </linearGradient>
             <clipPath id="clip0">
               <rect width="353" height="202" fill="white" />
@@ -74,19 +77,19 @@ class SignIn extends Component {
         </form>
 
         <svg
-          className="sign__bg sign__bg--bottom"
+          className={bemClass('bg', {bottom: true})}
           width="280"
           height="173"
           viewBox="0 0 280 173"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <g clip-path="url(#clip0)">
+          <g clipPath="url(#clip0)">
             <path
               d="M118.5 141.5C0.5 158 4.5 173.5 -10 225V254H301.5V7C289.833 8.83333 263.9 17.8 253.5 39C240.5 65.5 236.5 125 118.5 141.5Z"
               fill="url(#paint0_linear)"
               stroke="#21372A"
-              stroke-width="10"
+              strokeWidth="10"
             />
           </g>
           <defs>
@@ -98,8 +101,8 @@ class SignIn extends Component {
               y2="160.601"
               gradientUnits="userSpaceOnUse"
             >
-              <stop stop-color="#09C883" />
-              <stop offset="1" stop-color="#086A29" />
+              <stop stopColor="#09C883" />
+              <stop offset="1" stopColor="#086A29" />
             </linearGradient>
             <clipPath id="clip0">
               <rect width="280" height="173" fill="white" />
---
+++ src/config/bem.js
@@ -0,0 +1,3 @@
+import { withNaming } from "@bem-react/classname";
+const bem = withNaming({ n: "", e: "__", m: "--", v: "_" });
+export default bem;
