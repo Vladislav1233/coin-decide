acfc5c5e32db71ec43a997ebd5cfda18e4540c8e
Max Grishin <amed@ss.com>
2020-02-04 23:18:41 +0400

    Updates

---
+++ precache-manifest.a6ce514e4104205f1681faf67abbefd1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956449f126081831282ce0d56da9f3f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da3e386c5e5259d35a9c",
+    "url": "/static/css/2.5b391704.chunk.css"
+  },
+  {
+    "revision": "37267e59dda7a0fe08b7",
+    "url": "/static/css/main.0dad3fba.chunk.css"
+  },
+  {
+    "revision": "da3e386c5e5259d35a9c",
+    "url": "/static/js/2.264538dd.chunk.js"
+  },
+  {
+    "revision": "37267e59dda7a0fe08b7",
+    "url": "/static/js/main.1310d2f6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "17f69ab7f83b8b78a8cd23d1d36b2426",
+    "url": "/static/media/LogoNew.17f69ab7.svg"
+  },
+  {
+    "revision": "62ddfa37401ff6a3082742279d12c44c",
+    "url": "/static/media/arrow-side.62ddfa37.svg"
+  },
+  {
+    "revision": "296e7a16bde806b350e50168cb05b81e",
+    "url": "/static/media/beer.296e7a16.svg"
+  },
+  {
+    "revision": "15ee7bc4642d5b1b6ed719d0f39ddf3b",
+    "url": "/static/media/check-circle.15ee7bc4.svg"
+  },
+  {
+    "revision": "c9f2678a6bd055de8b4ed8cca9c0a00c",
+    "url": "/static/media/coin-v.c9f2678a.mp4"
+  },
+  {
+    "revision": "d1a23ceea3beebbff04b9f125e3ae766",
+    "url": "/static/media/list.d1a23cee.svg"
+  },
+  {
+    "revision": "0cf28634cd3d2ff1a2e9bb7996af8e7b",
+    "url": "/static/media/logo.0cf28634.png"
+  },
+  {
+    "revision": "8506d38a0854471cf37fa1063bfe7f69",
+    "url": "/static/media/phones.8506d38a.png"
+  },
+  {
+    "revision": "740a0b14b086bf2beb48ee922fa6c413",
+    "url": "/static/media/plug-for-bar.740a0b14.jpg"
+  },
+  {
+    "revision": "a33d9333d09cee94bb8a87336f515776",
+    "url": "/static/media/setting.a33d9333.svg"
+  },
+  {
+    "revision": "a316d86ea960f1d143123d04a4bee37f",
+    "url": "/static/media/swipe.a316d86e.svg"
+  },
+  {
+    "revision": "08eb5de922ba42350c2cb3d292cfae3d",
+    "url": "/static/media/taxi.08eb5de9.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coin-decide/precache-manifest.44d6104b9dd64ecdf1fe153bcb7668b1.js"
+  "/precache-manifest.a6ce514e4104205f1681faf67abbefd1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coin-decide/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
