b73950d9c3f33d0a7ca3ad6e640b9b96d8d3e732
Vladislav Dovzhenko <v_dovzhenko@flo.health>
2021-09-08 20:51:16 +0300

    редакс расширение заблочил

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,22 +1,21 @@
 // ref for create firebase: https://www.youtube.com/playlist?list=PL4cUxeGkcC9iWstfXntcj8f-dFZ4UtlN3
 
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
+import thunkMiddleware from "redux-thunk";
 import {
-  combineReducers,
-  createStore,
-  applyMiddleware,
-  compose
-} from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { reduxFirestore, getFirestore, firestoreReducer } from 'redux-firestore';
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
-import firebaseConfig from 'config/firebaseConfig';
-import { firebaseReducer } from 'react-redux-firebase';
+  reduxFirestore,
+  getFirestore,
+  firestoreReducer,
+} from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import firebaseConfig from "config/firebaseConfig";
+import { firebaseReducer } from "react-redux-firebase";
 
 // Note: reducers
-import promocodes from './promocodes';
-import auth from './auth';
-import bars from './bars';
-import users from './users';
+import promocodes from "./promocodes";
+import auth from "./auth";
+import bars from "./bars";
+import users from "./users";
 
 export default function rootReducerFunc(initialState = {}) {
   const rootReducer = combineReducers({
@@ -25,17 +24,19 @@ export default function rootReducerFunc(initialState = {}) {
     bars,
     users,
     firestore: firestoreReducer,
-    firebase: firebaseReducer
+    firebase: firebaseReducer,
   });
 
   return createStore(
     rootReducer,
     initialState,
     compose(
-      applyMiddleware(thunkMiddleware.withExtraArgument({ getFirebase, getFirestore })),
+      applyMiddleware(
+        thunkMiddleware.withExtraArgument({ getFirebase, getFirestore })
+      ),
       reactReduxFirebase(firebaseConfig, { attachAuthIsReady: true }),
-      reduxFirestore(firebaseConfig),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      reduxFirestore(firebaseConfig)
+      // window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : null // TODO: не работает если нет extension
     )
   );
 }
